82a83bf918e6ae06741428d3a165a3a0d0f4edc6
templantes <109276288+templantes@users.noreply.github.com>
2022-07-14 12:45:24 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?
+include('signup.php');
+?>
+<footer class="footer navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <div class="navbar-inner navbar-content-center" style="padding-top:15px;">
+            <ul class="navbar-left list-inline text-center text-muted credit"><li>&copy; 2022 <a href="http://website-hosting.eu.org"><small>website-hosting.eu.org</small></a></li></ul>
+            </ul>
+             <ul class="navbar-right list-inline text-center text-muted credit">
+                <li>
+            <!-- TOP.GE ASYNC COUNTER CODE -->
+            <div id="top-ge-counter-container" data-site-id="116078"></div>
+            <script async src="//counter.top.ge/counter.js"></script>
+            <!-- / END OF TOP.GE COUNTER CODE -->
+                </li>
+            </ul> 
+</div>
+</body>
+</html>
